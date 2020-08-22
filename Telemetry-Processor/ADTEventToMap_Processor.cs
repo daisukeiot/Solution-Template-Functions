e3edd0bb09f24bb1168f25e6f13ae4e85dbb6b16
Daisuke Nakahara <daisuken@msn.com>
2020-08-22 00:56:24 -0700

    Update ADTEventToMap_Processor.cs

--- Telemetry-Processor/ADTEventToMap_Processor.cs
+++ Telemetry-Processor/ADTEventToMap_Processor.cs
@@ -28,7 +28,7 @@ namespace Solution_Accelerator
             if (message["data"]["modelId"].ToString() == "dtmi:iotpnpadt:DigitalTwins:Space;1")
             {   //Set the ID of the room to be updated in your map. 
                 //Replace this line with your logic for retrieving featureID. 
-                string featureID = "UNIT103";
+                string featureID = "UNIT25";
 
                 //Iterate through the properties that have changed
                 foreach (var operation in message["data"]["patch"])
